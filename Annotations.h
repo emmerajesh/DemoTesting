0e6e401cff6ce143197cee596b4c6b504139e803
rajesh <rajesh@rajeshs-MacBook-Pro.local>
2015-01-22 12:56:32 +0530

    added nsobject class named "annotations"

---
+++ Annotations.h
@@ -0,0 +1,13 @@
+//
+//  Annotations.h
+//  Source
+//
+//  Created by rajesh on 22/01/15.
+//  Copyright (c) 2015 evv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Annotations : NSObject
+
+@end
---
+++ Annotations.m
@@ -0,0 +1,13 @@
+//
+//  Annotations.m
+//  Source
+//
+//  Created by rajesh on 22/01/15.
+//  Copyright (c) 2015 evv. All rights reserved.
+//
+
+#import "Annotations.h"
+
+@implementation Annotations
+
+@end
